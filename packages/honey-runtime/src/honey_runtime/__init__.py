5efd45f2e6619bd076e64835ac6c60d87a37cbe8
Jenway <jenwayhere@gmail.com>
2026-04-08 11:50:50 +0800

    runtime: remove unused mailbox router

--- packages/honey-runtime/src/honey_runtime/__init__.py
+++ packages/honey-runtime/src/honey_runtime/__init__.py
@@ -1,6 +1,5 @@
 """Canonical runtime routing exports."""
 
-from honey_runtime.routing.mailbox import NodeMailboxRouter
 from honey_runtime.routing.round_router import RoundProtocolRouter
 
-__all__ = ["NodeMailboxRouter", "RoundProtocolRouter"]
+__all__ = ["RoundProtocolRouter"]
